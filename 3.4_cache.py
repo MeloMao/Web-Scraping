031c8bd5f9a9634319abfa4316544be1a16ca180
Melo <837425931@qq.com>
2017-04-09 02:02:03 +0800

    Add files via upload

---
+++ 3.4_cache.py
@@ -0,0 +1,297 @@
+#coding=utf-8
+
+import os
+import re
+import urlparse
+import shutil
+import zlib
+import random
+import time
+import socket
+import urllib2
+import robotparser
+import datetime
+
+from datetime import datetime, timedelta
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+
+DEFAULT_AGENT = 'wswp'
+DEFAULT_DELAY = 5
+DEFAULT_RETRIES = 1
+DEFAULT_TIMEOUT = 60
+
+
+
+class Downloader:
+    def __init__(self, delay=DEFAULT_DELAY, user_agent=DEFAULT_AGENT, proxies=None, num_retries=DEFAULT_RETRIES, timeout=DEFAULT_TIMEOUT, opener=None, cache=None):
+        socket.setdefaulttimeout(timeout)
+        self.throttle = Throttle(delay)
+        self.user_agent = user_agent
+        self.proxies = proxies
+        self.num_retries = num_retries
+        self.opener = opener
+        self.cache = cache
+
+
+    def __call__(self, url):
+        result = None
+        if self.cache:
+            try:
+                result = self.cache[url]
+            except KeyError:
+                # url is not available in cache 
+                pass
+            else:
+                if self.num_retries > 0 and 500 <= result['code'] < 600:
+                    # server error so ignore result from cache and re-download
+                    result = None
+        if result is None:
+            # result was not loaded from cache so still need to download
+            self.throttle.wait(url)
+            proxy = random.choice(self.proxies) if self.proxies else None
+            headers = {'User-agent': self.user_agent}
+            result = self.download(url, headers, proxy=proxy, num_retries=self.num_retries)
+            if self.cache:
+                # save result to cache
+                self.cache[url] = result
+        return result['html']
+
+
+    def download(self, url, headers, proxy, num_retries, data=None):
+        print 'Downloading:', url
+        request = urllib2.Request(url, data, headers or {})
+        opener = self.opener or urllib2.build_opener()
+        if proxy:
+            proxy_params = {urlparse.urlparse(url).scheme: proxy}
+            opener.add_handler(urllib2.ProxyHandler(proxy_params))
+        try:
+            response = opener.open(request)
+            html = response.read()
+            code = response.code
+        except Exception as e:
+            print 'Download error:', str(e)
+            html = ''
+            if hasattr(e, 'code'):
+                code = e.code
+                if num_retries > 0 and 500 <= code < 600:
+                    # retry 5XX HTTP errors
+                    return self._get(url, headers, proxy, num_retries-1, data)
+            else:
+                code = None
+        return {'html': html, 'code': code}
+
+
+class Throttle:
+    """Throttle downloading by sleeping between requests to same domain
+    """
+    def __init__(self, delay):
+        # amount of delay between downloads for each domain
+        self.delay = delay
+        # timestamp of when a domain was last accessed
+        self.domains = {}
+        
+    def wait(self, url):
+        """Delay if have accessed this domain recently
+        """
+        domain = urlparse.urlsplit(url).netloc
+        last_accessed = self.domains.get(domain)
+        if self.delay > 0 and last_accessed is not None:
+            sleep_secs = self.delay - (datetime.now() - last_accessed).seconds
+            if sleep_secs > 0:
+                time.sleep(sleep_secs)
+        self.domains[domain] = datetime.now()
+
+
+
+def link_crawler(seed_url, link_regex=None, delay=5, max_depth=-1, max_urls=-1, user_agent='wswp', proxies=None, num_retries=1, scrape_callback=None, cache=None):
+    """Crawl from the given seed URL following links matched by link_regex
+    """
+    # the queue of URL's that still need to be crawled
+    crawl_queue = [seed_url]
+    # the URL's that have been seen and at what depth
+    seen = {seed_url: 0}
+    # track how many URL's have been downloaded
+    num_urls = 0
+    rp = get_robots(seed_url)
+    D = Downloader(delay=delay, user_agent=user_agent, proxies=proxies, num_retries=num_retries, cache=cache)
+
+    while crawl_queue:
+        url = crawl_queue.pop()
+        depth = seen[url]
+        # check url passes robots.txt restrictions
+        if rp.can_fetch(user_agent, url):
+            html = D(url)
+            links = []
+            if scrape_callback:
+                links.extend(scrape_callback(url, html) or [])
+
+            if depth != max_depth:
+                # can still crawl further
+                if link_regex:
+                    # filter for links matching our regular expression
+                    links.extend(link for link in get_links(html) if re.match(link_regex, link))
+
+                for link in links:
+                    link = normalize(seed_url, link)
+                    # check whether already crawled this link
+                    if link not in seen:
+                        seen[link] = depth + 1
+                        # check link is within same domain
+                        if same_domain(seed_url, link):
+                            # success! add this new link to queue
+                            crawl_queue.append(link)
+
+            # check whether have reached downloaded maximum
+            num_urls += 1
+            if num_urls == max_urls:
+                break
+        else:
+            print 'Blocked by robots.txt:', url
+
+
+
+def normalize(seed_url, link):
+    """Normalize this URL by removing hash and adding domain
+    """
+    link, _ = urlparse.urldefrag(link) # remove hash to avoid duplicates
+    return urlparse.urljoin(seed_url, link)
+
+
+def same_domain(url1, url2):
+    """Return True if both URL's belong to same domain
+    """
+    return urlparse.urlparse(url1).netloc == urlparse.urlparse(url2).netloc
+
+
+def get_robots(url):
+    """Initialize robots parser for this domain
+    """
+    rp = robotparser.RobotFileParser()
+    rp.set_url(urlparse.urljoin(url, '/robots.txt'))
+    rp.read()
+    return rp
+        
+
+def get_links(html):
+    """Return a list of links from html 
+    """
+    # a regular expression to extract all links from the webpage
+    webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
+    # list of all links from the webpage
+    return webpage_regex.findall(html)
+
+
+class DiskCache:
+    """
+    Dictionary interface that stores cached 
+    values in the file system rather than in memory.
+    The file path is formed from an md5 hash of the key.
+
+    >>> cache = DiskCache()
+    >>> url = 'http://example.webscraping.com'
+    >>> result = {'html': '...'}
+    >>> cache[url] = result
+    >>> cache[url]['html'] == result['html']
+    True
+    >>> cache = DiskCache(expires=timedelta())
+    >>> cache[url] = result
+    >>> cache[url]
+    Traceback (most recent call last):
+     ...
+    KeyError: 'http://example.webscraping.com has expired'
+    >>> cache.clear()
+    """
+
+    def __init__(self, cache_dir='cache', expires=timedelta(days=30), compress=True):
+        """
+        cache_dir: the root level folder for the cache
+        expires: timedelta of amount of time before a cache entry is considered expired
+        compress: whether to compress data in the cache
+        """
+        self.cache_dir = cache_dir
+        self.expires = expires
+        self.compress = compress
+
+    
+    def __getitem__(self, url):
+        """Load data from disk for this URL
+        """
+        path = self.url_to_path(url)
+        if os.path.exists(path):
+            with open(path, 'rb') as fp:
+                data = fp.read()
+                if self.compress:
+                    data = zlib.decompress(data)
+                result, timestamp = pickle.loads(data)
+                if self.has_expired(timestamp):
+                    raise KeyError(url + ' has expired')
+                return result
+        else:
+            # URL has not yet been cached
+            raise KeyError(url + ' does not exist')
+
+
+    def __setitem__(self, url, result):
+        """Save data to disk for this url
+        """
+        path = self.url_to_path(url)
+        folder = os.path.dirname(path)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        data = pickle.dumps((result, datetime.utcnow()))
+        if self.compress:
+            data = zlib.compress(data)
+        with open(path, 'wb') as fp:
+            fp.write(data)
+
+
+    def __delitem__(self, url):
+        """Remove the value at this key and any empty parent sub-directories
+        """
+        path = self._key_path(url)
+        try:
+            os.remove(path)
+            os.removedirs(os.path.dirname(path))
+        except OSError:
+            pass
+
+
+    def url_to_path(self, url):
+        """Create file system path for this URL
+        """
+        components = urlparse.urlsplit(url)
+        # when empty path set to /index.html
+        path = components.path
+        if not path:
+            path = '/index.html'
+        elif path.endswith('/'):
+            path += 'index.html'
+        filename = components.netloc + path + components.query
+        # replace invalid characters
+        filename = re.sub('[^/0-9a-zA-Z\-.,;_ ]', '_', filename)
+        # restrict maximum number of characters
+        filename = '/'.join(segment[:255] for segment in filename.split('/'))
+        return os.path.join(self.cache_dir, filename)
+
+
+    def has_expired(self, timestamp):
+        """Return whether this timestamp has expired
+        """
+        return datetime.utcnow() > timestamp + self.expires
+
+
+    def clear(self):
+        """Remove all the cached values
+        """
+        if os.path.exists(self.cache_dir):
+            shutil.rmtree(self.cache_dir)
+
+
+
+if __name__ == '__main__':
+    link_crawler('http://example.webscraping.com/', '/(index|view)', cache=DiskCache())
